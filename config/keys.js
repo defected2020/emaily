44e929902643ceb82ef644ca1b125cec48ff6e4d
George B <george@wizardofweb.com>
2021-12-11 21:02:10 +0800

    added keys

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {   
+    googleClientID: '872931087330-983dvflip4guk2fs5a6esf3spkhmkpm1.apps.googleusercontent.com',
+    googleClientSecret: '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g',
+    mongoURI: 'mongodb+srv://defected2021:MkkRK0qkQxhrEfaH@emaily.msg29.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    cookieKey: 'redacted',
+
+};
