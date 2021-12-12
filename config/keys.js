ea7ac421b23a772ea74e008dd1defb908bb4a2f8
George B <george@wizardofweb.com>
2021-12-12 19:11:23 +0800

    updated keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {   
     googleClientID: '872931087330-983dvflip4guk2fs5a6esf3spkhmkpm1.apps.googleusercontent.com',
-    googleClientSecret: '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g',
-    mongoURI: 'mongodb+srv://defected2021:MkkRK0qkQxhrEfaH@emaily.msg29.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://gwarrior:cycroteX6950@cluster0.msg29.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     cookieKey: 'redacted',
 
 };
--- services/passport.js
+++ services/passport.js
@@ -8,13 +8,15 @@ const User = mongoose.model('users');
 
 // new google strategy creates a new instance of the passport strategy.
 // Passport.use is like a generic register "passport I want you to be aware that a new strategy is availible to authenticate users inside of our application"
-passport.use(new GoogleStrategy({
+passport.use
+(new GoogleStrategy(
+    {
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, 
 (accessToken, refreshToken, profile, done) => {
-new User({ googleId: profile }).save //this creates a new instance of a user, it represents a record that might exist in our DB.(.save saves it to the DB)
+new User({ googleId: profile.id }).save //this creates a new instance of a user, it represents a record that might exist in our DB.(.save saves it to the DB)
 
 //access token is our oppertunity to now create a new user in our database with this identifying user information that was returned via google
 }
